23561522626d9e08c5825168a757c6c32b231725
Shubham Mishra <shubham.cmishra@gmail.com>
2026-01-21 16:15:06 -0800

    update keymap

--- lua/config/cmd-key.lua
+++ lua/config/cmd-key.lua
@@ -1,6 +1,8 @@
 return function(key)
-  if not vim.g.neovide then
-    return "<M-" .. key .. ">"
+  -- Use Command key on macOS with Neovide, Ctrl on Linux/Windows
+  local is_mac = vim.fn.has("macunix") == 1
+  if vim.g.neovide and is_mac then
+    return "<D-" .. key .. ">"
   end
-  return "<D-" .. key .. ">"
+  return "<C-" .. key .. ">"
 end

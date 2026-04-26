ce8784d3c0343829ca222168622c927b7a273c11
Shubham Mishra <shubham.cmishra@gmail.com>
2026-04-26 13:16:52 -0700

    Add new languages

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "eslint", "ts_ls", "svelte", "lua_ls" },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      vim.diagnostic.config({
-        virtual_text = {
-          prefix = "●",
-        },
-      })
-      lspconfig.eslint.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.svelte.setup({})
-      lspconfig.lua_ls.setup({})
-    end,
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "eslint", "ts_ls", "svelte", "lua_ls", "hls", "pyright" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "●",
+				},
+			})
+			lspconfig.eslint.setup({})
+			lspconfig.ts_ls.setup({})
+			lspconfig.svelte.setup({})
+			lspconfig.lua_ls.setup({})
+		end,
+	},
 }
